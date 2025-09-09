7e292514be4f7f1567e2d287cf72537729d65d18
金澤 裕 <ykanazawa@ntv-wands.co.jp>
2025-09-09 22:24:12 +0900

    削除

--- app/contract/detail/page.tsx
+++ app/contract/detail/page.tsx
@@ -14,7 +14,6 @@ import {
   BreadcrumbPage,
   BreadcrumbSeparator,
 } from "@/components/ui/breadcrumb"
-import { LicenseGrant } from "@/components/license-grant"
 
 export default function Page() {
   return (
@@ -48,7 +47,6 @@ export default function Page() {
             </Breadcrumb>
           </div>
         </header>
-        <LicenseGrant />
         <div className="flex flex-1 flex-col">
           <div className="@container/main flex flex-1 flex-col gap-2">
             <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">

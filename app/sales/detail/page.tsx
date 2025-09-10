af53e3778298f426acf469fedca42937e66aeed1
金澤 裕 <ykanazawa@ntv-wands.co.jp>
2025-09-10 12:06:23 +0900

    テキスト修正

--- app/sales/detail/page.tsx
+++ app/sales/detail/page.tsx
@@ -49,7 +49,7 @@ export default function Page() {
         <div className="flex flex-1 flex-col">
           <div className="@container/main flex flex-1 flex-col gap-2">
             <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6 px-4 lg:px-6">
-              詳細
+              準備中
             </div>
           </div>
         </div>
--- app/sales/page.tsx
+++ app/sales/page.tsx
@@ -45,7 +45,7 @@ export default function Page() {
         <div className="flex flex-1 flex-col">
           <div className="@container/main flex flex-1 flex-col gap-2">
             <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6 px-4 lg:px-6">
-              一覧
+              準備中
             </div>
           </div>
         </div>
